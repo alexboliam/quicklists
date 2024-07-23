6d637a0fc057b9bbb2bf1980d34b928e702f38b7
alexboliam <alexboliam@gmail.com>
2024-07-23 22:10:26 +0000

    Deploying to gh-pages from @ alexboliam/quicklists@a69c08d299bdc4ca9a447584fa3c61fa7edf49f4 🚀

--- src/app/shared/interfaces/checklist.ts
+++
@@ -1,9 +0,0 @@
-export interface Checklist {
-  id: string;
-  title: string;
-  dateCreated: Date;
-}
-
-export type AddChecklist = Omit<Checklist, 'id'|'dateCreated'>;
-export type EditChecklist = { id: Checklist['id'], data: AddChecklist };
-export type RemoveChecklist = Checklist['id'];
--- src/app/shared/utils/guid.helper.ts
+++
@@ -1,7 +0,0 @@
-import { Guid } from "guid-typescript";
-
-export const GuidHelper = {
-  CreateGuid(length = 8) {
-    return Guid.create().toString().slice(0, length);
-  }
-};
